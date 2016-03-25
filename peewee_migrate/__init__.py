5e008c9e79077cb3f690bcb758e2de6dcba980b4
Nick Shobe <nickshobe@gmail.com>
2016-03-24 21:02:47 -0500

    Bumping version for full feature release + minor for last bug fix

--- peewee_migrate/__init__.py
+++ peewee_migrate/__init__.py
@@ -6,7 +6,7 @@
 # Package information
 # ===================
 
-__version__ = "0.0.9"
+__version__ = "0.1.1"
 __project__ = "peewee_migrate"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "BSD"
