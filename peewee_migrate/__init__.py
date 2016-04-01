9f8c7b8081e03e9b0f3d27cc899e3b6dc048de3e
Kirill Klenov <horneds@gmail.com>
2016-04-01 13:51:50 -0700

    Bump version: 0.3.2 → 0.3.3

--- peewee_migrate/__init__.py
+++ peewee_migrate/__init__.py
@@ -9,7 +9,7 @@ import datetime as dt
 # Package information
 # ===================
 
-__version__ = "0.3.2"
+__version__ = "0.3.3"
 __project__ = "peewee_migrate"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "BSD"
