5951afcd034671732ff34064054a56be23dbc7f2
Kirill Klenov <horneds@gmail.com>
2017-09-07 13:08:41 +0300

    Bump version: 0.13.0 → 0.13.1

--- peewee_migrate/__init__.py
+++ peewee_migrate/__init__.py
@@ -11,7 +11,7 @@ import peewee as pw
 # Package information
 # ===================
 
-__version__ = "0.13.0"
+__version__ = "0.13.1"
 __project__ = "peewee_migrate"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "BSD"
