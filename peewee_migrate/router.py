ab7278104b3ffb0af6a578542b91cc8ac1561405
Taylor Brazelton <taylor.r.brazelton@gmail.com>
2019-06-14 17:09:13 -0400

    Update router.py
    Updated so that we are not adding a new untested code branch.

--- peewee_migrate/router.py
+++ peewee_migrate/router.py
@@ -85,8 +85,7 @@ class BaseRouter(object):
                 models = [m for module in modules for m in load_models(module)]
 
             except ImportError as e:
-                if not auto:
-                    return self.logger.error(e.msg)
+                self.logger.error(e.msg)
                 return self.logger.error("Can't import models module: %s", auto)
 
             if self.ignore:
