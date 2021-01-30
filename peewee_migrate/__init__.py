dcecec177172ab786440155d92632cef1e76e789
Kirill Klenov <horneds@gmail.com>
2021-01-30 23:41:32 +0300

    build(version): 1.3.0 -> 1.4.0

--- peewee_migrate/__init__.py
+++ peewee_migrate/__init__.py
@@ -11,7 +11,7 @@ import peewee as pw
 # Package information
 # ===================
 
-__version__ = "1.3.0"
+__version__ = "1.4.0"
 __project__ = "peewee_migrate"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "BSD"
