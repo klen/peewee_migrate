d3e66ada4853e8e8cb30935a637b0724609b9a78
Kirill Klenov <horneds@gmail.com>
2021-10-20 16:39:02 +0300

    feat: support python 3.10

--- setup.py
+++ setup.py
@@ -1,4 +1,29 @@
+"""Setup the package."""
+
+
+# Parse requirements
+# ------------------
+import pkg_resources
+import pathlib
+
+
+def parse_requirements(path: str) -> 'list[str]':
+    with pathlib.Path(path).open() as requirements:
+        return [str(req) for req in pkg_resources.parse_requirements(requirements)]
+
+
+# Setup package
+# -------------
+
 from setuptools import setup
 
 
-setup()
+setup(
+    install_requires=parse_requirements('requirements/requirements.txt'),
+    extras_require={
+        'tests': parse_requirements('requirements/requirements-tests.txt'),
+        'build': ['bump2version', 'wheel'],
+    }
+)
+
+# pylama:ignore=E402,D
